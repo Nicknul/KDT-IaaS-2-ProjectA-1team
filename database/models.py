db7e4f2412d8068ccb12db83d12e9d1addf14291
정호연 <jea7730@gmail.com>
2024-08-08 11:55:25 +0900

    :egg: 정호연 : table data DTO

--- database/models.py
+++ database/models.py
@@ -1,17 +1,8 @@
 from pydantic import BaseModel
-from typing import List, Optional
-
-class SearchRequest(BaseModel):
-    name: str  # name 필드는 문자열 타입이다.
-
-class UserDTO(BaseModel):
-    이름: str  # 이름 필드는 문자열 타입이다.
-    아이디: str  # 아이디 필드는 문자열 타입이다.
-    비밀번호: int  # 비밀번호 필드는 정수 타입이다.
+from typing import List, Dict
 
 class TableRequest(BaseModel):
-    table: str  # table 필드는 문자열 타입이다.
-    name: Optional[str] = None  # name 필드는 선택적 문자열 타입이다.
+    table: str
 
 class CreateTableRequest(BaseModel):
     tableName: str
