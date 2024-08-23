4bd09a97b11f0b9cc5e7ed33f3b6dc9e6c71f994
정호연 <jea7730@gmail.com>
2024-08-23 15:02:58 +0900

    :truck: 정호연 : Total Styles code

---
+++ frontnext/src/app/ui/Header/styles/styles.ts
@@ -0,0 +1,12 @@
+export const Header =
+  'h-16 flex items-center justify-between bg-gray-100 relative shadow-lg';
+export const HeaderLeft = 'flex items-center mr-4';
+export const HeaderCenter = 'flex flex-grow justify-end';
+export const HeaderRight = 'flex items-center ml-4 pr-2.5';
+export const HeaderButton =
+  'px-4 py-2 w-36 h-16 bg-gray-100 text-gray-700 cursor-pointer hover:bg-gray-200';
+export const HeaderButtonFirst =
+  'px-4 py-2 w-36 h-16 bg-gray-100 text-gray-700 cursor-pointer hover:bg-gray-200';
+export const HeaderButtonLogout =
+  'flex items-center justify-center text-sm font-bold bg-gray-100 text-gray-700 underline';
+export const HeaderSpan = 'text-base font-bold text-gray-700 mr-4';
