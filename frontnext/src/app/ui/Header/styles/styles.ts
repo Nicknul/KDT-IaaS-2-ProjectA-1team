f82031e115613f7298488e24bf8dd55463ba8396
정호연 <jea7730@gmail.com>
2024-08-23 15:31:30 +0900

    :egg: 정호연 : common styles 조립

--- frontnext/src/app/ui/Header/styles/styles.ts
+++ frontnext/src/app/ui/Header/styles/styles.ts
@@ -1,12 +1,14 @@
+import {
+  ButtonBackgroundColors,
+  NoButtonRound,
+  FlexRow,
+} from '../../styles/CommonStyles';
+
 export const Header =
   'h-16 flex items-center justify-between bg-gray-100 relative shadow-lg';
 export const HeaderLeft = 'flex items-center mr-4';
 export const HeaderCenter = 'flex flex-grow justify-end';
 export const HeaderRight = 'flex items-center ml-4 pr-2.5';
-export const HeaderButton =
-  'px-4 py-2 w-36 h-16 bg-gray-100 text-gray-700 cursor-pointer hover:bg-gray-200';
-export const HeaderButtonFirst =
-  'px-4 py-2 w-36 h-16 bg-gray-100 text-gray-700 cursor-pointer hover:bg-gray-200';
-export const HeaderButtonLogout =
-  'flex items-center justify-center text-sm font-bold bg-gray-100 text-gray-700 underline';
-export const HeaderSpan = 'text-base font-bold text-gray-700 mr-4';
+export const HeaderButton = `w-36 h-16 text-gray-700 ${NoButtonRound} ${ButtonBackgroundColors('gray', 100)}`;
+export const HeaderButtonLogout = `${FlexRow} text-sm font-bold bg-gray-100 text-gray-700 underline`;
+export const HeaderSpan = 'font-bold text-gray-700 mr-4';
