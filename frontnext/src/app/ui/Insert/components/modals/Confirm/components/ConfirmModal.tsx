1eeaac375abb4d9ea1dd27042786117335297d5f
정호연 <jea7730@gmail.com>
2024-08-23 15:59:03 +0900

    :egg: 정호연 : 스타일 변수명 수정

--- frontnext/src/app/ui/Insert/components/modals/Confirm/components/ConfirmModal.tsx
+++ frontnext/src/app/ui/Insert/components/modals/Confirm/components/ConfirmModal.tsx
@@ -1,7 +1,7 @@
 // components/ConfirmModal.tsx
 import React from 'react';
 import Modal from '@/app/ui/Modal-Test/modalComponent';
-import { closeButton, confirmButton } from '@/app/ui/styles/ButtonStyles';
+import { GrayButton, BlueButton } from '@/app/ui/styles/ButtonStyles';
 import { ConfirmModalProps } from './ModalProps';
 
 interface ConfirmModalContentProps extends ConfirmModalProps {
@@ -18,10 +18,10 @@ const ConfirmModal: React.FC<ConfirmModalContentProps> = ({
     <Modal show={show} onClose={onClose}>
       <h2 className="text-lg font-bold mb-4">{message}</h2>
       <div className="flex justify-end space-x-4">
-        <button className={closeButton} onClick={onClose}>
+        <button className={GrayButton} onClick={onClose}>
           취소
         </button>
-        <button className={confirmButton} onClick={onConfirm}>
+        <button className={BlueButton} onClick={onConfirm}>
           확인
         </button>
       </div>
