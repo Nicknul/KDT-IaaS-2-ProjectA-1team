1ddb0f27a319efe24d687dd71cb1f52350eecefd
정호연 <jea7730@gmail.com>
2024-08-23 17:01:45 +0900

    :construction_worker: 정호연 : 테스트 파일

--- frontnext/tailwind.tsx
+++ frontnext/tailwind.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const stylesTest = () => {
   return (
     <>
-      <div className="bg-blue-500 rounded-lg py-2 px-4 text-white shadow"></div>
+      <div className="text-red"></div>
     </>
   );
 };
