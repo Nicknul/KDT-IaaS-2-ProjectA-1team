422cb185150ba812293035bdd9f1aa2d1ae5d11c
정호연 <jea7730@gmail.com>
2024-08-26 10:27:12 +0900

    :rocket: 정호연 : Sidebar styles file

---
+++ frontnext/src/app/ui/Insert/components/Sidebar/styles/SidebarStyles.ts
@@ -0,0 +1,7 @@
+// Sidebar Styles
+export const sidebar =
+  'w-72 bg-gradient-to-b from-gray-200 to-gray-300 flex flex-col p-4 space-y-4';
+export const SidebarTableItem = 'py-2 px-4';
+export const SidebarContainer = 'flex';
+export const SidebarHeight = 'h-[calc(100vh-4rem)]';
+export const SidebarButtonContainer = 'space-x-4 mt-4';
