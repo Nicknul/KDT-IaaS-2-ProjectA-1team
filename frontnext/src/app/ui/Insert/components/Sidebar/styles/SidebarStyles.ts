e56da6a6b92388b42e0628d524ad3f7a83fccc44
정호연 <jea7730@gmail.com>
2024-08-26 10:47:04 +0900

    :fire: 정호연 : 미사용 스타일 코드 삭제

--- frontnext/src/app/ui/Insert/components/Sidebar/styles/SidebarStyles.ts
+++ frontnext/src/app/ui/Insert/components/Sidebar/styles/SidebarStyles.ts
@@ -1,7 +1,4 @@
 // Sidebar Styles
 export const sidebar =
-  'w-72 bg-gradient-to-b from-gray-200 to-gray-300 flex flex-col p-4 space-y-4';
-export const SidebarTableItem = 'py-2 px-4';
-export const SidebarContainer = 'flex';
-export const SidebarHeight = 'h-[calc(100vh-4rem)]';
-export const SidebarButtonContainer = 'space-x-4 mt-4';
+  'w-72 bg-gray-300 flex flex-col p-4 space-y-4 h-[calc(100vh-4rem)]';
+export const SidebarButtonContainer = 'space-x-5';
