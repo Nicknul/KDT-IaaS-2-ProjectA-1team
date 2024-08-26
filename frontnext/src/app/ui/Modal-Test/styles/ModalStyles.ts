f365c7510871da1e8dbf9f8e3d6da7015d762a9d
정호연 <jea7730@gmail.com>
2024-08-26 11:20:59 +0900

    :rocket: 정호연 : Modal Styles file

---
+++ frontnext/src/app/ui/Modal-Test/styles/ModalStyles.ts
@@ -0,0 +1,8 @@
+// Modal Styles
+export const ModalOverlay =
+  'fixed inset-0 flex items-center justify-center bg-black z-50 bg-opacity-50';
+export const ModalContent =
+  'p-6 rounded-lg relative max-w-lg w-full mx-4 backdrop-blur-lg bg-white/80';
+export const ModalText = 'text-gray-800 text-lg';
+export const ConfirmButton =
+  'bg-blue-500 hover:bg-blue-600 mt-4 px-4 py-2 rounded-lg text-white';
