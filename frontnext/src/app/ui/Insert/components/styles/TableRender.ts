4b3396a9d4544319fa122caf1b176da4000199a2
정호연 <jea7730@gmail.com>
2024-08-26 12:56:57 +0900

    :rocket: 정호연 : insert table data rendering styles file

---
+++ frontnext/src/app/ui/Insert/components/styles/TableRender.ts
@@ -0,0 +1,15 @@
+import { transparent } from '@/app/ui/styles/CommonStyles';
+
+// insert table rendering style
+export const TableContainer = `overflow-x-auto max-h-[780px] overflow-y-auto`;
+export const table = `min-w-full bg-white`;
+export const thead = ``;
+export const th = `relative px-4 py-2 border-b-2 border-gray-200 bg-gray-100 text-left text-sm font-semibold text-gray-600`;
+export const DeleteColumnButton = `absolute top-1/2 right-1 transform -translate-y-1/2 bg-red-500 text-white text-sm rounded-full w-6 h-6 flex items-center justify-center hover:bg-red-600`;
+export const tbody = ``;
+export const tr = `hover:bg-gray-100`;
+export const td = `relative px-4 py-2 border-b border-gray-200 text-sm text-gray-700`;
+export const DeleteRowButton = `absolute left-0 top-1/2 transform -translate-y-1/2 bg-red-500 text-white text-sm rounded-full w-6 h-6 flex items-center justify-center hover:bg-red-600`;
+export const ErrorText = `text-red-500 text-xs mt-1`;
+export const TableWrapper = `min-w-full overflow-x-auto`;
+export const InputWidth = `w-32 px-2 py-1 border rounded`;
