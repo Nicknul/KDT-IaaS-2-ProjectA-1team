26d1c41828d88209abba4bce117a900d796c348b
정호연 <jea7730@gmail.com>
2024-08-26 11:45:18 +0900

    :egg: 정호연 : flex style 모듈 조립

--- frontnext/src/app/ui/Insert/components/Sidebar/styles/SidebarStyles.ts
+++ frontnext/src/app/ui/Insert/components/Sidebar/styles/SidebarStyles.ts
@@ -1,3 +1,3 @@
 // Sidebar Styles
-export const sidebar =
-  'w-72 bg-gray-300 flex flex-col p-4 space-y-4 h-[calc(100vh-4rem)]';
+import { FlexColumn } from '@/app/ui/styles/CommonStyles';
+export const sidebar = `w-72 h-[calc(100vh-4rem)] bg-gray-300 ${FlexColumn} p-4 space-y-4`;
