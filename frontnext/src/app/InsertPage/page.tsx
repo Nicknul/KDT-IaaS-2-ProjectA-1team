517a744a17fd1bd307b8654ef2e1c6a919919913
정호연 <jea7730@gmail.com>
2024-08-26 10:51:15 +0900

    :egg: 정호연 : 모듈명 수정

--- frontnext/src/app/InsertPage/page.tsx
+++ frontnext/src/app/InsertPage/page.tsx
@@ -1,14 +1,14 @@
 'use client';
 import Image from 'next/image';
 import HeaderSection from '../ui/Header/headerSection';
-import TotalSidebar from '../ui/Insert/pages/TotalInsert';
+import TotalInsert from '../ui/Insert/pages/TotalInsert';
 import { LanguageProvider } from '../ui/SettingMoules/LanguageContext';
 
 export default function Home() {
   return (
     <LanguageProvider>
       <HeaderSection />
-      <TotalSidebar />
+      <TotalInsert />
     </LanguageProvider>
   );
 }
