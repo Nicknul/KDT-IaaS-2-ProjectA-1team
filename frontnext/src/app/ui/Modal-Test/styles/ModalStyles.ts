ac19c76a3d7fafcbb26b706db009fd2e0a1401cf
정호연 <jea7730@gmail.com>
2024-08-26 14:24:35 +0900

    :egg: 정호연 : modal styles 수정

--- frontnext/src/app/ui/Modal-Test/styles/ModalStyles.ts
+++ frontnext/src/app/ui/Modal-Test/styles/ModalStyles.ts
@@ -1,8 +1,9 @@
-// Modal Styles
-export const ModalOverlay =
-  'fixed inset-0 flex items-center justify-center bg-black z-50 bg-opacity-50';
-export const ModalContent =
-  'p-6 rounded-lg relative max-w-lg w-full mx-4 backdrop-blur-lg bg-white/80';
+// Modal Component Style
+import { FlexRow } from '../../styles/CommonStyles';
+import { BlueButton } from '../../styles/ButtonStyles';
+
+export const ModalOverlay = `fixed inset-0 ${FlexRow} bg-black/50 z-50`;
+export const ModalWindow =
+  'p-6 rounded-lg relative max-w-lg w-full mx-4 backdrop-blur-lg bg-white/80 text-center';
 export const ModalText = 'text-gray-800 text-lg';
-export const ConfirmButton =
-  'bg-blue-500 hover:bg-blue-600 mt-4 px-4 py-2 rounded-lg text-white';
+export const ConfirmButton = `${BlueButton}`;
